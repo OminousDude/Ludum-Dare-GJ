11797f2ab5965e6918927379255cb395f55682c1
Anas Barbouch <anas.barbouch@polymtl.ca>
2023-09-29 22:30:19 -0400

    added enemy bounce/"hit" and spawner

---
+++ Ludum Dare/Assets/Scripts/EnemyBounce.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBounce : MonoBehaviour
+{
+    public bool shouldBounce = true;
+    private int bounceCount = 0;
+
+    [SerializeField]
+    private Rigidbody2D rigidBody;
+
+    private Vector2 velocity;
+
+    private void Update()
+    {
+        velocity = rigidBody.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (shouldBounce && collision.gameObject.layer == 6)
+        {
+            float speed = velocity.magnitude;
+
+            Vector2 direction = Vector2.Reflect(velocity.normalized, collision.contacts[0].normal);
+
+            rigidBody.velocity = direction * Mathf.Max(speed, 0);
+        } else if (shouldBounce && collision.gameObject.layer == 3)
+        {
+            shouldBounce = false;
+        }
+    }
+}
