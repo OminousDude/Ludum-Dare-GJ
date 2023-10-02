16e0b021a55b38171b2699a403c2a3089688506c
FarahZkr <farahzakari@gmail.com>
2023-10-02 19:07:27 -0400

    Time delta

--- Ludum Dare/Assets/Scripts/EnemyHealthScript.cs
+++ Ludum Dare/Assets/Scripts/EnemyHealthScript.cs
@@ -50,7 +50,7 @@ public class EnemyHealthScript : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject.Find("Player").GetComponentInChildren<PlayerMovement>().hitWaitTime = 350;
+            GameObject.Find("Player").GetComponentInChildren<PlayerMovement>().hitWaitTime = 3 * Time.deltaTime;
             if (canBeHit)            
             {
                 time = 0;
